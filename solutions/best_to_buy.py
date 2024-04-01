fea23ae33d730fc8d35cd4a7163d5b531d76c9e2
eredden <redden.evan@gmail.com>
2024-04-01 10:43:16 -0400

    fixed minor error with list type capitalization

--- solutions/best_to_buy.py
+++ solutions/best_to_buy.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/1220170661/
 
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices: list[int]) -> int:
         cheapest   = 10001
         max_profit = 0
 
