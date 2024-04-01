6cbee8f1bbdbabc49405d861cb151908cb99b4f5
eredden <redden.evan@gmail.com>
2024-04-01 10:42:46 -0400

    added best to buy and sell stocks

---
+++ solutions/best_to_buy.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/1220170661/
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        cheapest   = 10001
+        max_profit = 0
+
+        for i in range(0, len(prices)): 
+            if cheapest > prices[i]:
+                cheapest = prices[i]
+            else:
+                profit = prices[i] - cheapest
+                max_profit = profit if profit > max_profit else max_profit
+
+        return max_profit
